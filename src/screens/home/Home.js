fcc5482c8cbde11dfbe085d72e3f0c74c3f06576
Siva <sivansbhfe@gmail.com>
2019-10-12 14:17:56 +0530

    Added 'Home' component

---
+++ src/screens/home/Home.js
@@ -0,0 +1,10 @@
+import React, {Component} from './node_modules/react';
+import './Home.css';
+
+class Home extends Component{
+    render(){
+        return(<div>From Home</div>});
+    }
+}
+
+export default Home;
