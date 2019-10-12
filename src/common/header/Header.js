95c7693664670e13ca726066abf7c77c4b68349e
Siva <sivansbhfe@gmail.com>
2019-10-12 15:17:14 +0530

    Added logo and logo animation

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import './Header.css';
+import logo from '../../assets/logo.svg';
 
 //From Material-UI
 import Button from '@material-ui/core/Button';
@@ -9,10 +10,11 @@ class Header extends Component{
         return(
             <div>
             <header className="app-header">
+                <img src={logo} className="app-logo" alt="logo"/>
                 <div className="login-button">
                     <Button variant="contained" color="default">
                         Login
-                    </Button>
+                    </Button>   
                 </div>
             </header>  
         </div>);
