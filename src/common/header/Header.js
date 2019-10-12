66db70d469330cadd8e4774d2042039b54ffbf23
Siva <sivansbhfe@gmail.com>
2019-10-12 15:11:25 +0530

    Created and formatted header

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -7,9 +7,15 @@ import Button from '@material-ui/core/Button';
 class Header extends Component{
     render(){   
         return(
-        <div>
-            <Button color="default" variant="conained">Login</Button>
-      </div>);
+            <div>
+            <header className="app-header">
+                <div className="login-button">
+                    <Button variant="contained" color="default">
+                        Login
+                    </Button>
+                </div>
+            </header>  
+        </div>);
     }
 }
 
