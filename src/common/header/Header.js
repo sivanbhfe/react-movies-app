6378f9578915bf8c2ad8c1fcfb4cdd43efb34ad4
Siva <sivansbhfe@gmail.com>
2019-10-13 09:05:36 +0530

    Added username and password inside Login form

--- src/common/header/Header.js
+++ src/common/header/Header.js
@@ -7,45 +7,82 @@ import Modal from 'react-modal';
 import Button from '@material-ui/core/Button';
 import Tabs from '@material-ui/core/Tabs';
 import Tab from '@material-ui/core/Tab';
+import Typography from '@material-ui/core/Typography';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
 
-class Header extends Component{
-    constructor(){
-        super();
-        this.state= {modalIsOpen:false, value:0};
-    }
+const customStyles = {
+content:{
+top:'50%',
+left:'50%',
+right: 'auto',
+bottom: 'auto',
+marginRight: '-50%',
+transform: 'translate(-50%, -50%)'
+}
+};
 
-    modalOpenHandler = ()=>{
-        this.setState({modalIsOpen: true});
-    }
-
-    modalCloseHandler = ()=>{
-        this.setState({modalIsOpen: false});
-    }
-
-    tabChangeHandler = (event, value) => {
-        this.setState({value});
-    }
-
-    render(){   
-        return(
-            <div>
-            <header className="app-header">
-                <img src={logo} className="app-logo" alt="logo"/>
-                <div className="login-button">
-                    <Button onClick={this.modalOpenHandler} variant="contained" color="default">
-                        Login
-                    </Button>   
-                </div>
-            </header>  
-            <Modal ariaHideApp={false}isOpen={this.state.modalIsOpen} contentLabel="Login" 
-            onRequestClose={this.modalCloseHandler}>
-                <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
-                    <Tab label="Login" />
-                    <Tab label="Register" />
-                </Tabs>
-            </Modal>
-        </div>);
-    }
+const TabContainer = function(props){
+return(<Typography component="div" style={{padding:0}}>
+    {props.children}
+</Typography>); 
 }
 
+// const Tellit = function (props){
+//     return(<p>{props.id}</p>);
+// }
+
+class Header extends Component{
+
+constructor(){
+super();
+this.state= {modalIsOpen:false, value:0};
+}
+
+modalOpenHandler = ()=>{
+this.setState({modalIsOpen: true});
+}
+
+modalCloseHandler = ()=>{
+this.setState({modalIsOpen: false});
+}
+
+tabChangeHandler = (event, value) => {
+this.setState({value});
+}
+
+render(){   
+return(
+    <div>
+    <header className="app-header">
+        <img src={logo} className="app-logo" alt="logo"/>
+        <div className="login-button">
+            <Button onClick={this.modalOpenHandler} variant="contained" color="default">
+                Login
+            </Button>   
+        </div>
+    </header>  
+    <Modal style={customStyles} ariaHideApp={false} isOpen={this.state.modalIsOpen} contentLabel="Login" 
+    onRequestClose={this.modalCloseHandler}>
+        <Tabs value={this.state.value} onChange={this.tabChangeHandler}>
+            <Tab label="Login" />
+            <Tab label="Register" />
+        </Tabs>
+        <TabContainer>
+            <FormControl required>
+                <InputLabel htmlFor="userName">UserName</InputLabel>
+                <Input id="userName" type="text"/>
+            </FormControl>
+            <FormControl required>
+                <InputLabel htmlFor="password">Password</InputLabel>
+                <Input id="password" type="password"/>
+            </FormControl>
+        </TabContainer>
+  
+    </Modal>
+</div>);
+}
+}
+  /* <Tellit id="TOOOOOOOOOOOOOR"><p>OOOOOOOOOOOOOOOOPS/p></p></Tellit> */
 export default Header;
