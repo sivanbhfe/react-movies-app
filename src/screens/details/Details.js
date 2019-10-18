ee5bc74b00e860eb95aec3500ef0d9f2f8bea54a
Siva <sivansbhfe@gmail.com>
2019-10-18 06:42:01 +0530

    Added back button and video trailer container in details page

--- src/screens/details/Details.js
+++ src/screens/details/Details.js
@@ -1,6 +1,9 @@
 import React, {Component} from 'react';
+import YouTube from 'react-youtube';
+import ReactDOM from 'react-dom';
 import './Details.css';
 import Header from '../../common/header/Header';
+import Home from '../home/Home';
 import moviesData from '../../assets/movieData';
 import Typography from '@material-ui/core/Typography';
 
@@ -21,10 +24,26 @@ class Details extends Component {
         console.log(this.state);
     }
 
+    backtohomeHandler =() =>
+    {
+        ReactDOM.render(<Home/>, document.getElementById("root"));
+    }
+
 render(){
     let movie = this.state.movie;
+    const opts = {
+        height: '300',
+        width: '700',
+        playerVars: {autoplay: 1}
+    }
+
     return(<div className="details">
         <Header/>
+        <div className="back">
+            <Typography onClick={this.backtohomeHandler}>
+                &#60; Back to Home
+            </Typography>
+        </div>
         <div className="flex-containerDetails">
             <div className="leftDetails">
                 <img src={movie.poster_url} alt={movie.title}/>
@@ -40,6 +59,14 @@ render(){
                     <Typography><span className="bold">Release Date: </span>{movie.release_date}</Typography>
                     <Typography><span className="bold">Rating: </span>{movie.critics_rating}</Typography><br />
                     <Typography><span className="bold">Plot: </span><a href={movie.wiki_url}>(Wiki Link)</a><span> </span>{movie.storyline}</Typography>
+                    <div className="trailer-container">
+                    <Typography><span className="bold">Trailer: </span></Typography>
+                    <YouTube
+                    videoId={movie.trailer_url.split("?v=")[1]}
+                    opts={opts}
+                    onReady={this._onReady}>
+                    </YouTube>
+                    </div>
                 </div>
             </div>
             <div className="rightDetails">
