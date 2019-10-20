7db8c1ede30de3181086aad55f8b481bff669a15
Siva <sivansbhfe@gmail.com>
2019-10-20 10:18:50 +0530

    Added BookShow filter card and location filter

--- src/screens/bookshow/BookShow.js
+++ src/screens/bookshow/BookShow.js
@@ -6,6 +6,10 @@ import ReactDOM from 'react-dom';
 import Header from '../../common/header/Header';
 import Home from '../home/Home';
 import Details from '../details/Details';
+import language from '../../common/language';
+import location from '../../common/location';
+import showDate from '../../common/showDate';
+import showTime from '../../common/showTime';
 
 //CSS files
 import './BookShow.css';
@@ -14,8 +18,23 @@ import './BookShow.css';
 import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
+import FormControl from '@material-ui/core/FormControl';
+import InputLabel from '@material-ui/core/InputLabel';
+import Input from '@material-ui/core/Input';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import ListItemText from '@material-ui/core/ListItemText';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 class BookShow extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            location: ""
+        }
+    }
 
 backtohomeHandler =() =>
 {
@@ -33,15 +52,29 @@ render(){
                 &#60; Back to Movie Details
             </Typography>
             <Card className="cardStyle">
-                <CardContent>
-                <Typography variant="headline" component="h2">
-                BOOK SHOW
-            </Typography><br />
-                </CardContent>
-            </Card>
+                        <CardContent>
+                            <Typography variant="headline" component="h2">
+                                BOOK SHOW
+                            </Typography><br />
+
+                            <FormControl required className="formControl">
+                                <InputLabel htmlFor="location">Choose Location:</InputLabel>
+                                <Select
+                                    value={this.state.location}
+                                    onChange={this.locationChangeHandler}
+                                >
+                                    {location.map(loc => (
+                                        <MenuItem key={"loc" + loc.id} value={loc.location}>
+                                            {loc.location}
+                                        </MenuItem>
+                                    ))}
+                                </Select>
+                            </FormControl>
+                        </CardContent>
+                    </Card>
         </div>
         </div>
-    );
+    )
 }
 }
 export default BookShow;
